a7f8f1a3cee8eae0ffcbe0c5bd0b43c85159ba51
Leilani <leilani.ray@mymail.nwtc.edu>
2017-10-15 12:41:16 -0500

    accept test project?

---
+++ Steganography with AES Encryption/ImageSteganographyWithEncryptionTest/AEStest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageSteganographyWithEncryptionTest
+{
+    [TestClass]
+    public class AEStest
+    {
+        /* Tests will be built here. Behaviors to be tested include:
+         * 1. Will we be testing here to makes sure standard characters are used in the message entered?
+         * 2. I'm not actually sure what tests need to be done.
+         */
+
+        // unit test method
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            // act
+            // assert
+        }
+    }
+}
