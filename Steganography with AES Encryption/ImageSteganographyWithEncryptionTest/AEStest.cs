67739fbb9aa99d1f6823f11af1b407f7685c1dd5
Leilani <leilani.ray@mymail.nwtc.edu>
2017-10-17 12:39:24 -0500

    just added comments and am practicing another commit.

--- Steganography with AES Encryption/ImageSteganographyWithEncryptionTest/AEStest.cs
+++ Steganography with AES Encryption/ImageSteganographyWithEncryptionTest/AEStest.cs
@@ -9,6 +9,10 @@ namespace ImageSteganographyWithEncryptionTest
         /* Tests will be built here. Behaviors to be tested include:
          * 1. Will we be testing here to makes sure standard characters are used in the message entered?
          * 2. I'm not actually sure what tests need to be done.
+         * Ideas from Chris M:
+         * There's a Queue<string> bytesList in the ImageDecoder class that should only ever contain strings of binary numbers.
+         * The ImageEncoder has a Color sanitizedColor. The RGB values should always be even numbers, never odd.
+         * Check that the alpha channel of every pixel on the rawImage is the same as on the encodedImage.
          */
 
         // unit test method
