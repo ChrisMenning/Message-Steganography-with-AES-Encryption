ea35ecb7ea2707666d31357fa1c2067a6bfcd929
hoeggy <32176788+hoeggy@users.noreply.github.com>
2017-10-20 14:13:07 -0500

    Updated classes with SyleCop and created new class for character computing

---
+++ Steganography with AES Encryption/Steganography with AES Encryption/Classes/CharacterCompute.cs
@@ -0,0 +1,22 @@
+﻿//----------------------------------------------------------------------------------
+// <copyright file="CharacterCompute.cs" company="Legendary Lichens">
+//    © Legendary Lichens. All rights reserved. 
+//    2017 - Nathan Beyer / Chris Hoegger / Chris Menning / Leilani Ray
+// </copyright>
+//---------------------------------------------------------------------------------- 
+
+namespace Steganography_with_AES_Encryption.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The Character Compute class
+    /// </summary>
+    public class CharacterCompute
+    {
+    }
+}
--- Steganography with AES Encryption/Steganography with AES Encryption/Program.cs
+++ Steganography with AES Encryption/Steganography with AES Encryption/Program.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿//----------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Legendary Lichens">
+//    © Legendary Lichens. All rights reserved. 
+//    2017 - Nathan Beyer / Chris Hoegger / Chris Menning / Leilani Ray
+// </copyright>
+//---------------------------------------------------------------------------------- 
 
 namespace Steganography_with_AES_Encryption
 {
-    static class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    public class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
