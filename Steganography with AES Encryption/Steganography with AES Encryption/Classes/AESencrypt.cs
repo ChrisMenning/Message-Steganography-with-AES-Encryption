3b7d4a1e510f1e2f175d9a7c247fc2f143c7de44
ChrisMenning <chrismenning@gmail.com>
2017-10-30 09:05:52 -0500

    Added ASCII Art Footer to the following files: AESDecrypt AESEncrypt ImageDecoder ImageEncoder JpegEncoder PasswordHandler

--- Steganography with AES Encryption/Steganography with AES Encryption/Classes/AESencrypt.cs
+++ Steganography with AES Encryption/Steganography with AES Encryption/Classes/AESencrypt.cs
@@ -160,3 +160,28 @@ namespace Steganography_with_AES_Encryption
         }
     }
 }
+
+/***
+ *                                                                                                 
+ *                                                            ,,                                   
+ *    `7MMF'                                                `7MM                                   
+ *      MM                                                    MM                                   
+ *      MM         .gP"Ya   .P"Ybmmm .gP"Ya `7MMpMMMb.   ,M""bMM   ,6"Yb.  `7Mb,od8 `7M'   `MF'    
+ *      MM        ,M'   Yb :MI  I8  ,M'   Yb  MM    MM ,AP    MM  8)   MM    MM' "'   VA   ,V      
+ *      MM      , 8M""""""  WmmmP"  8M""""""  MM    MM 8MI    MM   ,pm9MM    MM        VA ,V       
+ *      MM     ,M YM.    , 8M       YM.    ,  MM    MM `Mb    MM  8M   MM    MM         VVV        
+ *    .JMMmmmmMMM  `Mbmmd'  YMMMMMb  `Mbmmd'.JMML  JMML.`Wbmd"MML.`Moo9^Yo..JMML.       ,V         
+ *                         6'     dP                                                   ,V          
+ *                         Ybmmmd'                                                  OOb"           
+ *                                                                                                 
+ *                  ,,          ,,                                                                 
+ *    `7MMF'        db        `7MM                                                                 
+ *      MM                      MM                                                                 
+ *      MM        `7MM  ,p6"bo  MMpMMMb.  .gP"Ya `7MMpMMMb.  ,pP"Ybd                               
+ *      MM          MM 6M'  OO  MM    MM ,M'   Yb  MM    MM  8I   `"                               
+ *      MM      ,   MM 8M       MM    MM 8M""""""  MM    MM  `YMMMa.                               
+ *      MM     ,M   MM YM.    , MM    MM YM.    ,  MM    MM  L.   I8                               
+ *    .JMMmmmmMMM .JMML.YMbmd'.JMML  JMML.`Mbmmd'.JMML  JMML.M9mmmP'                               
+ *                                                                                                 
+ *                                                                                                 
+ */
