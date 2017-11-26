d0722bb3b7ad35586e93e1c4716e6a36a95658e1
ChrisMenning <chrismenning@gmail.com>
2017-11-26 15:03:47 -0600

    Stylecopped the password input dialog.

--- Steganography with AES Encryption/Steganography with AES Encryption/PasswordInputDialog.cs
+++ Steganography with AES Encryption/Steganography with AES Encryption/PasswordInputDialog.cs
@@ -1,31 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿//----------------------------------------------------------------------------------
+// <copyright file="PasswordInputDialog.cs" company="Legendary Lichens">
+//    © Legendary Lichens. All rights reserved. 
+//    2017 - Nathan Beyer / Chris Hoegger / Chris Menning / Leilani Ray
+// </copyright>
+//---------------------------------------------------------------------------------- 
 
 namespace Steganography_with_AES_Encryption
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The PasswordInputDialog form class. Extends the Form class.
+    /// </summary>
     public partial class PasswordInputDialog : Form
     {
-        FormMain main;
+        /// <summary>
+        /// A reference to the main form.
+        /// </summary>
+        private FormMain main;
 
+        /// <summary>
+        /// Initializes a new instance of the PasswordInputDialog object. 
+        /// </summary>
+        /// <param name="mainform">A reference to the main form.</param>
         public PasswordInputDialog(FormMain mainform)
         {
-
-            InitializeComponent();
+            this.InitializeComponent();
 
             this.CenterToParent();
             this.main = mainform;
-            this.AcceptButton = buttonOK;
+            this.AcceptButton = this.buttonOK;
         }
 
-
-        private void buttonOK_Click(object sender, EventArgs e)
+        /// <summary>
+        /// The click event for the OK button.
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">The event</param>
+        private void ButtonOK_Click(object sender, EventArgs e)
         {
             this.main.Password = textBoxInput.Text;
             this.Close();
