3f9ff6ac2f7d2cc31199064e71998ce53ac37463
ChrisMenning <chrismenning@gmail.com>
2017-12-02 11:31:27 -0600

    Refactored to simply LSB-getting. Changed from using a method to using a modulus in-line.

--- Steganography with AES Encryption/Steganography with AES Encryption/Classes/BitmapDecoder.cs
+++ Steganography with AES Encryption/Steganography with AES Encryption/Classes/BitmapDecoder.cs
@@ -112,16 +112,6 @@ namespace Steganography_with_AES_Encryption
         }
 
         /// <summary>
-        /// Accepts a byte, determines if it is even or odd, and adds either a 1 or 0 to the bytesFromImage list.
-        /// </summary>
-        /// <param name="colorChannel">A byte that should come from a color channel</param>
-        /// <returns>The last bit of any byte.</returns>
-        public int LastBitFromColorChannel(byte colorChannel)
-        {
-            return colorChannel % 2;
-        }
-
-        /// <summary>
         /// Accepts a bitmap and decodes the message hidden in the LSB of each color channel of each pixel.
         /// </summary>
         /// <param name="encoded">The bitmap with a hidden message in it.</param>
@@ -139,10 +129,10 @@ namespace Steganography_with_AES_Encryption
                     Color pixelColor = this.EncodedImage.GetPixel(column, row);
 
                     // Pull the last bit out of each color channel and concatenate them onto the bitsFromImage list.
-                    this.bitsFromImage.Add(this.LastBitFromColorChannel(pixelColor.A));
-                    this.bitsFromImage.Add(this.LastBitFromColorChannel(pixelColor.R));
-                    this.bitsFromImage.Add(this.LastBitFromColorChannel(pixelColor.G));
-                    this.bitsFromImage.Add(this.LastBitFromColorChannel(pixelColor.B));
+                    this.bitsFromImage.Add(pixelColor.A % 2);
+                    this.bitsFromImage.Add(pixelColor.R % 2);
+                    this.bitsFromImage.Add(pixelColor.G % 2);
+                    this.bitsFromImage.Add(pixelColor.B % 2);
                 }
             }
 
